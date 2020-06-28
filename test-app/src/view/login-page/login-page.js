bd021db938d1126848d479749be0a891209571c5
efnushtaev <efnushtaev@gmail.com>
2020-06-28 14:30:15 +0300

    authrization realized

--- test-app/src/view/login-page/login-page.js
+++ test-app/src/view/login-page/login-page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import {onUserPasswordChange, onUserEmailChange, onUserDataCheck} from './../../redux/reducers/auth-reducer';
 
 const LoginPage = (props) => {
@@ -9,20 +10,28 @@ const LoginPage = (props) => {
   }
   const {email, password, isAuthorized, onUserEmailChange, onUserPasswordChange, onUserDataCheck} = props
   return (
-    <form onSubmit={handleSubmit}>
-        <label>
-          Email (testuser@mail.com)
-        <input defaultValue={email} id="user" name="email" type="text" onChange={e => onUserEmailChange(e.target.value)}/>
-        </label>
-        <label>
-          Логин (testuser)
-          <input defaultValue={password} id="user" name="user" type="text" onChange={e => onUserPasswordChange(e.target.value)}/>
-        </label>
-        <label>
-          Войти
-          <input type="submit" value="Submit"/>
-        </label>
-      </form>
+    isAuthorized 
+      ? <Redirect from="/login" to="/posts"/>
+      : <div className={"container"}>
+      <div className={"row justify-content-md-center"}>
+        <div className={"col col-12 col-md-6"}>
+          <form className={"m-5"} onSubmit={handleSubmit}>
+            <div className={"form-group"}>
+                <label className={"mt-3"} for="login">
+                  Login (testuser)
+                </label>
+                <input className={"form-control"} defaultValue={email} id="login" name="email" type="text" onChange={e => onUserEmailChange(e.target.value)}/>
+                <label className={"mt-3"} for="password">
+                  Логин (testuser)
+                </label>
+                <input className={"form-control"} defaultValue={password} id="password" name="user" type="text" onChange={e => onUserPasswordChange(e.target.value)}/>
+                <input className={"form-control btn btn-primary mt-5 block-d"} type="submit" value="Войти" id="submit"/>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
