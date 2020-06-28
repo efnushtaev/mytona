4a14acbf1e8d105f5d0a3a6e283a1c6e2073db7a
efnushtaev <efnushtaev@gmail.com>
2020-06-28 14:29:26 +0300

    navigation added

--- test-app/src/view/navigation-bar/navigation-bar.js
+++ test-app/src/view/navigation-bar/navigation-bar.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {updateAuthorization} from './../../redux/reducers/auth-reducer'
 
-const NavigationBar = (props) => {
+const NavigationBar = ({isAuthorized, updateAuthorization}) => {
   return (
-    <h1>NavigationBar</h1>
+    <div>
+      <h4 className={"text-info"}>Efim Nushtaev</h4>
+      <ul className={"nav justify-content-end"}>
+        <li className={"nav-item"}>
+          <NavLink activeClassName={"active"} className={"nav-link"} to="/">Main</NavLink>
+        </li>
+        <li className={"nav-item"}>
+          <NavLink activeClassName={"active"} className={"nav-link"} to="/posts">Posts</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName={"active"} className={"nav-link"} to="/albums">Albums</NavLink>
+        </li>
+        <li>
+          {isAuthorized
+            ? <button className={"btn btn-link nav-link border-left text-dark"} onClick={() => updateAuthorization(false)}>Logout</button>
+            : <NavLink activeClassName={"active"} className={"nav-link border-left"} to="/login">Login</NavLink>
+          }
+        </li>
+      </ul>
+    </div>
   )
 }
 
-export default NavigationBar
+let mapStateToProps = (state) => ({
+  isAuthorized: state.auth.isAuthorized
+})
+
+export default connect(mapStateToProps, {updateAuthorization})(NavigationBar)
