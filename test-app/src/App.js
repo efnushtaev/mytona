0933c2fc2c478ace3793756883204b8d257890d5
efnushtaev <efnushtaev@gmail.com>
2020-06-28 14:29:40 +0300

    redirection realized

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import LoginPage from './view/login-page/login-page';
 import MainPage from './view/main-page/main-page';
 import AlbumsPage from './view/albums-page/albums-page';
 import PostsPage from './view/posts-page/posts-page';
 import NavigationBar from './view/navigation-bar/navigation-bar';
+import { connect } from 'react-redux';
 
-const App = () => {
+const App = ({isAuthorized}) => {
+  
   return (
-    <div className="AppContainer">
+    <div className="container-fluid mt-3">
       <div>
         <NavigationBar/>
       </div>
       <div>
       <Switch>
-        <Route path="/albums" render={() => <AlbumsPage/>} />
-        <Route path="/posts" render={() => <PostsPage/>} />
+        {isAuthorized 
+          ? <Route path="/albums" render={() => <AlbumsPage/>} />
+          : <Redirect from="/albums" to="/login"/>
+        }
+        {isAuthorized 
+          ? <Route path="/posts" render={() => <PostsPage/>} />
+          : <Redirect from="/posts" to="/login"/>
+        }
         <Route path="/login" render={() => <LoginPage/>} />
-        <Route path="/" render={() => <MainPage/>} />
+        <Route exact path="/" render={() => <MainPage/>} />
         <Route path="*" render={() => <h1>404</h1>} />
       </Switch>
 
       </div>
-
-
-
     </div>
   );
 }
 
-export default App;
+let mapStateToProps = (state) => ({
+  isAuthorized: state.auth.isAuthorized
+})
+export default connect(mapStateToProps, {})(App);
