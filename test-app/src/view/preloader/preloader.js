102e1551d629de7018d21f64178426d3c682b783
efnushtaev <efnushtaev@gmail.com>
2020-06-29 19:19:03 +0300

    preloader realized

---
+++ test-app/src/view/preloader/preloader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Preloader = () => {
+  return <div class="spinner-border" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>
+}
+
+export default Preloader
