e042d0cdf6c4687aa981356b39ebbb6da29ecbee
efnushtaev <efnushtaev@gmail.com>
2020-06-30 16:09:47 +0300

    Updates

---
+++ precache-manifest.efbdb0922c9f069c122805b7e540e3bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc61e96a6d5ddfb4e13157cccfea582e",
+    "url": "/mytona/index.html"
+  },
+  {
+    "revision": "2bee1f2608c080e2f4ed",
+    "url": "/mytona/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a7b4331e8c45d328cac7",
+    "url": "/mytona/static/js/2.bb1ad720.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mytona/static/js/2.bb1ad720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bee1f2608c080e2f4ed",
+    "url": "/mytona/static/js/main.4f0cf124.chunk.js"
+  },
+  {
+    "revision": "83cbaea8f6ceab812c4c",
+    "url": "/mytona/static/js/runtime-main.b4d58fb2.js"
+  }
+]);
